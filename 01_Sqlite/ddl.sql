7cd7306a83ed54defc0c091990bd840006409405
Shineast <canfrog@naver.com>
2025-09-09 14:11:26 +0900

    VIEWS

--- 01_Sqlite/ddl.sql
+++ 01_Sqlite/ddl.sql
@@ -1,3 +1,4 @@
+-- CREATE TABLE
 create table movies (
   movie_id integer primary key AUTOINCREMENT,
   title TEXT UNIQUE NOT NULL,
@@ -8,4 +9,21 @@ create table movies (
   for_kids INTEGER NOT NULL DEFAULT 0 check (for_kids = 0 or for_kids = 1)
 ) strict;
 
+-- DROP TABLE
 drop table movies
+
+-- CREATE VIEW
+CREATE VIEW v_flop_or_not AS
+SELECT 
+  CASE WHEN revenue < budget THEN 'FLOP' ELSE 'SUCCESS' END AS is_success,
+  count(*) AS total_movies
+FROM movies 
+WHERE budget IS NOT NULL
+      AND revenue IS NOT NULL
+GROUP BY is_success
+ORDER BY total_movies DESC
+
+SELECT * FROM v_flop_or_not;
+
+-- DROP VIEW
+DROP VIEW v_flop_or_not;
