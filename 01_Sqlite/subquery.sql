29ff3dd82b664315fd7bc9888e04c147b9a0b4a7
Shineast <canfrog@naver.com>
2025-09-10 18:15:35 +0900

    CTE (COMMON TABLE EXPRESSIONS

--- 01_Sqlite/subquery.sql
+++ 01_Sqlite/subquery.sql
@@ -10,3 +10,21 @@ FROM movies
 WHERE rating > (
   SELECT AVG(rating) FROM movies
 )
+
+-- CTE
+WITH avg_revenue_cte AS (
+  SELECT AVG(revenue) FROM movies
+), avg_rating_cte AS (
+  SELECT AVG(rating) FROM movies
+)
+  
+SELECT 
+  title, 
+  director,
+  revenue,
+  rating,
+  ROUND((SELECT * FROM avg_revenue_cte), 0) AS avg_revenue,
+  (SELECT * FROM avg_rating_cte) AS avg_rating
+from movies 
+where revenue > (SELECT * FROM avg_revenue_cte)
+      AND rating > (SELECT * FROM avg_rating_cte)
