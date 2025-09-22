7c33e1acc4bc7f17a9e2e014e25952a9d413883b
Shineast <canfrog@naver.com>
2025-09-22 23:15:31 +0900

    FOREIGN KEYS

---
+++ 02_MySQL/foreign_keys.sql
@@ -0,0 +1,34 @@
+INSERT INTO
+    breeds (name, size_category, typical_lifespan)
+VALUES ('Golden Retriever', 'big', 12);
+
+INSERT INTO
+    owners (name, email, phone, address)
+VALUES (
+        'Adam Smith',
+        'adam@smith.com',
+        '1122334455',
+        '9010 St. Scotland'
+    );
+
+INSERT INTO
+    dogs (name, date_of_birth, weight, breed_id, owner_id)
+VALUES ('Buddy', '2018-03-15', 10.5, 1, 1);
+
+DROP TABLE dogs;
+
+CREATE TABLE dogs (
+    dog_id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    date_of_birth DATE,
+    weight DECIMAL(5, 2),
+    owner_id BIGINT UNSIGNED,
+    breed_id BIGINT UNSIGNED,
+    FOREIGN KEY (owner_id) REFERENCES owners (owner_id),
+    CONSTRAINT breed_fk FOREIGN KEY (breed_id) REFERENCES breeds (breed_id)
+);
+
+-- error
+INSERT INTO
+    dogs (name, date_of_birth, weight, breed_id, owner_id)
+VALUES ('Champ', '2018-03-15', 10.5, 1, 1);
