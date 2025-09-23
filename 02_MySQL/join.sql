ac2793039af5c98f890141efff60edec81511d19
Shineast <canfrog@naver.com>
2025-09-24 01:08:32 +0900

    INNER JOIN

--- 02_MySQL/join.sql
+++ 02_MySQL/join.sql
@@ -1,2 +1,12 @@
 -- CROSS JOIN 
 SELECT * FROM dogs CROSS JOIN owners;
+
+-- INNER JOIN 
+SELECT 
+  dogs.name as dog_name,
+  owners.name as owner_name,
+  breeds.name as breed_name
+FROM dogs 
+  INNER JOIN owners ON dogs.owner_id = owners.owner_id
+  -- INNER JOIN with USING
+  INNER JOIN breeds USING (breed_id)
