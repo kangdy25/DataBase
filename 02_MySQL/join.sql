2ebd25baed0fee4713741cc6e2c7201028aeaf62
Shineast <canfrog@naver.com>
2025-09-24 01:15:19 +0900

    OUTER JOIN

--- 02_MySQL/join.sql
+++ 02_MySQL/join.sql
@@ -10,3 +10,13 @@ FROM dogs
   INNER JOIN owners ON dogs.owner_id = owners.owner_id
   -- INNER JOIN with USING
   INNER JOIN breeds USING (breed_id)
+
+-- OUTER JOIN 
+
+-- LEFT OUTER JOIN, RIGHT OUTER JOIN
+SELECT 
+  dogs.name as dog_name,
+  owners.name as owner_name
+FROM dogs 
+  LEFT OUTER JOIN owners ON dogs.owner_id = owners.owner_id  
+  RIGHT OUTER JOIN owners ON dogs.owner_id = owners.owner_id
