ffa98a767cbe53b6c2912572816907a2d5171d0f
Shineast <canfrog@naver.com>
2025-09-25 01:10:25 +0900

    JOINS PRACTICE 2

--- 02_MySQL/quiz.sql
+++ 02_MySQL/quiz.sql
@@ -46,3 +46,58 @@ SELECT
 FROM dogs
   LEFT JOIN dog_tricks using (dog_id)
 WHERE (dog_tricks.dog_id IS NULL)
+
+-- 7. Show all breeds and the count of dogs for each breed
+SELECT 
+  breeds.name,
+  count(*) AS dog_count
+FROM breeds
+  JOIN dogs using(breed_id)
+GROUP BY breeds.name
+
+-- 8. Display all owners with
+-- the count of their dogs,
+-- the average dog weight and the average dog age.
+SELECT 
+  owners.name,
+  count(dogs.dog_id) AS count_dog,
+  AVG(dogs.weight) AS avg_weight,
+  AVG(TIMESTAMPDIFF(YEAR, dogs.date_of_birth, CURDATE())) AS avg_ages
+FROM owners
+  LEFT JOIN dogs using (owner_id)
+GROUP BY owners.owner_id
+
+-- 9. Show all tricks and the number
+-- of dogs that know each trick
+-- ordered by popularity
+SELECT
+  tricks.name AS tricks_name,
+  count(*) AS popularity
+FROM tricks
+  JOIN dog_tricks using (trick_id)
+GROUP BY tricks_name
+ORDER BY popularity DESC
+  
+-- 10. Display all dogs along with
+-- the count of tricks they know
+SELECT 
+  dogs.name AS dog_name,
+  count(trick_id) AS known_tricks
+FROM dogs
+  JOIN dog_tricks using (dog_id)
+  JOIN tricks using (trick_id)
+GROUP BY dog_id
+ORDER BY known_tricks DESC
+
+-- 11. List all owner with their
+-- dogs and the tricks their dogs know
+SELECT 
+  owners.name AS owner_name,
+  dogs.name AS own_dogs,
+  dog_tricks.proficiency,
+  tricks.difficulty
+FROM owners
+  JOIN dogs using (owner_id)
+  JOIN dog_tricks using (dog_id)
+  JOIN tricks using (trick_id)
+ORDER BY owner_name
