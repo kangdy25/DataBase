8fac55a006c1a09e66b82a8214c878e91d64225b
Shineast <canfrog@naver.com>
2025-10-01 20:19:08 +0900

    RETURN TYPES

--- 03_PostgreSQL/functions.sql
+++ 03_PostgreSQL/functions.sql
@@ -9,3 +9,21 @@ LANGUAGE SQL
 DROP FUNCTION hello_world();
 
 SELECT title, hello_world('ang', 'gimotti') FROM movies;
+
+-- Return Types
+CREATE OR REPLACE FUNCTION is_hit_or_flop(movie movies)
+RETURNS TABLE (hit_or_flop TEXT, other_thing numeric) AS 
+$$
+	SELECT CASE
+		WHEN movie.revenue >= movie.budget THEN 'Hit'
+		WHEN movie.revenue < movie.budget THEN 'Flop'
+		ELSE 'N/A'
+	END, 1111
+$$
+LANGUAGE SQL 
+
+
+SELECT
+	title,
+	(is_hit_or_flop(movies.*)).*
+FROM movies
