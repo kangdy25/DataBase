c74f995e746855767005e80cf54271f2f7e3a721
Shineast <canfrog@naver.com>
2025-10-01 20:56:58 +0900

    PROCEDURE

---
+++ 03_PostgreSQL/procedure.sql
@@ -0,0 +1,42 @@
+-- Procedure
+CREATE PROCEDURE set_zero_revenue() AS 
+$$
+	UPDATE movies SET revenue = NULL WHERE revenue = 0;
+$$
+LANGUAGE SQL;
+
+CALL set_zero_revenue();
+
+-- Procedure with return value
+CREATE PROCEDURE hello_world_p(IN name TEXT, OUT greeting TEXT) AS
+$$
+	BEGIN
+		greeting = 'Hello ' || name;
+	END
+$$
+LANGUAGE plpgsql;
+
+CALL hello_world_p('dongyoon', NULL)
+
+-- PL/pgSQL
+CREATE PROCEDURE hello_world_i(IN name TEXT, IN lang TEXT, OUT greeting TEXT) AS
+$$
+DECLARE 
+	spanish_hello TEXT := 'hola';	
+	italian_hello TEXT := 'ciao';	
+	korean_hello TEXT := '안냥';
+BEGIN 
+	IF lang = 'korean' THEN
+		greeting := korean_hello || ' ' || name || '!!';
+	ELSIF lang = 'italian' THEN
+		greeting := italian_hello || ' ' || name || '!!';
+	ELSIF lang = 'spanish' THEN
+		greeting := spanish_hello || ' ' || name || '!!';
+	ELSE
+		greeting := 'hello ' || name || '!!';
+	END IF;
+END;
+$$
+LANGUAGE plpgsql;
+ 
+CALL hello_world_i('Dongyoon', 'japanese', NULL)
