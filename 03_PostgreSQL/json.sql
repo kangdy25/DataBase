517b1f360db1b337a692f2765fd97a87531a75a0
Shineast <canfrog@naver.com>
2025-10-10 23:25:18 +0900

    QUERYING JSON

--- 03_PostgreSQL/json.sql
+++ 03_PostgreSQL/json.sql
@@ -13,3 +13,23 @@ INSERT INTO users (profile) VALUES
 (json_build_object('name', 'Giga', 'age', 25, 'city', 'Tbilisi', 'hobbies', json_build_array('reading', 'climbing')));
 
 SELECT * FROM users
+
+-- Data Extraction
+SELECT 
+	profile ->> 'name' AS name,
+	profile ->> 'city' AS city,
+	profile -> 'age' AS age,
+	profile -> 'hobbies' ->> 0 AS first_hobby
+FROM users
+
+
+-- Querying JSON
+SELECT 
+	profile ->> 'name' AS name,
+	profile ->> 'city' AS city,
+	jsonb_array_length(profile -> 'hobbies') AS total_hobbies
+FROM users
+WHERE profile ->> 'name' = 'Taco' AND 
+	(profile ->> 'age')::integer <= 30 AND
+	profile -> 'city' ? 'Budapest' AND
+	profile ->> 'city' LIKE 'B%'
