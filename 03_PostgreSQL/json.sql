de4ab51e7774e031c2803ecfbe3f6c33ba692935
Shineast <canfrog@naver.com>
2025-10-10 23:47:19 +0900

    PROCESSING JSON

--- 03_PostgreSQL/json.sql
+++ 03_PostgreSQL/json.sql
@@ -33,3 +33,34 @@ WHERE profile ->> 'name' = 'Taco' AND
 	(profile ->> 'age')::integer <= 30 AND
 	profile -> 'city' ? 'Budapest' AND
 	profile ->> 'city' LIKE 'B%'
+
+-- Processing JSON
+
+-- profile에 email 추가
+UPDATE users 
+SET profile = profile || jsonb_build_object('email', 'x@x.com');
+
+-- name이 Giga인 profile에서 email 제거
+UPDATE users 
+SET profile = profile - 'email'
+WHERE profile ->> 'name' = 'Giga';
+
+-- name이 Taco인 profile에 hobbies 배열 추가
+UPDATE users 
+SET profile = profile || jsonb_build_object('hobbies', jsonb_build_array('climbing', 'traveling')) 
+WHERE profile ->> 'name' = 'Taco';
+
+-- profile의 hobbies 배열에서 'climbing' 제거
+UPDATE users 
+SET profile = profile || jsonb_set(profile, '{hobbies}', (profile -> 'hobbies') - 'climbing');
+
+-- hobbies 배열 확인
+SELECT profile -> 'hobbies' FROM users;
+
+-- profile의 hobbies 배열에 'cooking' 추가
+UPDATE users 
+SET profile = profile || jsonb_set(profile, '{hobbies}', (profile -> 'hobbies') || jsonb_build_array('cooking'));
+
+SELECT profile -> 'hobbies' FROM users;
+
+SELECT * FROM users;
